12a8aea8f78cf00ad7425f31aa0f7f45f4cc21aa
liasushis30 <liasushipetshop30@gmail.com>
2023-10-12 21:40:57 -0600

    subir website
    he subido todos mis archivos

---
+++ menu.js
@@ -0,0 +1,17 @@
+(function(){
+    const openbutton = document.querySelector (' .nav_menu');
+    const menu = document.querySelector ('.nav_link');
+    const Closemenu = document.querySelector ('.nav_close');
+
+    openButto.addEventListener('click, ()=>'{
+        menu.classList.add('nav_link--show');
+    }); 
+  
+    Closemenu.addEventListener('click , () =>'{
+        menu.classList.add('nav_link--show');}
+    }); 
+
+
+
+
+})(); 
